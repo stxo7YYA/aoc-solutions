40e9a89d37642f896b363b8086b4d9bdddac5117
stxo7YYA <119558047+stxo7YYA@users.noreply.github.com>
2022-12-01 07:10:06 +0000

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,14 +1,12 @@
 f = open('input', 'r')
-maximum = 0
 calories = 0
 top = []
 for line in f.readlines():
     if line == '\n':
-        maximum = max(maximum, calories)
         top.append(calories)
         calories = 0
         continue
     calories += int(line)
 top.sort()
-print('Part One', maximum)
+print('Part One', max(top))
 print('Part Two', sum(top[-3:]))
