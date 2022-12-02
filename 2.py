00e5d699dd67841eef3f43ca5030562f7a673d77
stxo7YYA <119558047+stxo7YYA@users.noreply.github.com>
2022-12-02 06:33:03 +0000

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,3 @@
-def rot(n, r):
-    n += r
-    return 3 if not n else 1 if n > 3 else n
-
 f = open('input', 'r')
 one = 0
 two = 0
@@ -9,17 +5,9 @@ for line in f.readlines():
     if line == '\n':
         break
     x, y = line.split()
-    x = ord(x) - ord('A') + 1
-    y = ord(y) - ord('X') + 1
-    for i in range(-1, 2):
-        if x == rot(y, i):
-            one += [6, 3, 0][i + 1] + y
-    match y:
-        case 1:
-            two += 0 + rot(x, -1)
-        case 2:
-            two += 3 + rot(x, 0)
-        case 3:
-            two += 6 + rot(x, +1)
+    x = ord(x) - ord('A')
+    y = ord(y) - ord('X')
+    one += [7, 4, 1][(1 + x - y) % 3] + y
+    two += [1 + (x - 1)%3, 4 + x, 7 + (x + 1)%3][y]
 print('part One', one)
 print('Part Two', two)
