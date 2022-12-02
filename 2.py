67e0ab1e7beaa5d952da031f3ac66572818f7197
stxo7YYA <119558047+stxo7YYA@users.noreply.github.com>
2022-12-02 07:07:36 +0000

    Update 2.py

--- 2.py
+++ 2.py
@@ -9,5 +9,5 @@ for line in f.readlines():
     y = ord(y) - ord('X')
     one += [7, 4, 1][(1 + x - y) % 3] + y
     two += [1 + (x - 1)%3, 4 + x, 7 + (x + 1)%3][y]
-print('part One', one)
+print('Part One', one)
 print('Part Two', two)
