fcb657c1ba08435e70c96635adecf8b3b78bd0e7
stxo7YYA <119558047+stxo7YYA@users.noreply.github.com>
2022-12-04 09:43:42 +0000

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,16 +1,10 @@
 f = open('input', 'r')
-lines = [line.strip() for line in f][:-1]
-one = 0
-two = 0
-for line in lines:
-    a, b = line.split(',')
-    a = [int(x) for x in a.split('-')]
-    b = [int(x) for x in b.split('-')]
-    if b[0] <= a[0] and b[1] >= a[1]\
-    or a[0] <= b[0] and a[1] >= b[1]:
+one = two = 0
+for line in [*f][:-1]:
+    (a, b), (x, y) = [[int(x) for x in x.split('-')] for x in line.split(',')]
+    if x <= a and y >= b or a <= x and b >= y:
         one += 1
-    if a[0] >= b[0] and a[0] <= b[1]\
-    or b[0] >= a[0] and b[0] <= a[1]:
+    if a >= x and a <= y or x >= a and x <= b:
         two += 1
 print('Part One', one)
 print('Part Two', two)
