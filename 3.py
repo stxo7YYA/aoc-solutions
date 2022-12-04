9550e25982485538228c6c05a8074569e7a5d562
stxo7YYA <119558047+stxo7YYA@users.noreply.github.com>
2022-12-04 06:29:45 +0000

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,16 +1,11 @@
 f = open('input', 'r')
-l = [x.strip() for x in f.readlines()][:-1]
+l = [x.strip() for x in f][:-1]
 f = lambda c: ord(c) - 96 if c.islower() else ord(c) - 38
 one = 0
 two = 0
-for ln in l:
-    s1 = set(); [s1.add(c) for c in ln[:len(ln)//2]]
-    s2 = set(); [s2.add(c) for c in ln[len(ln)//2:]]
-    one += f((s1 & s2).pop())
-print('Part One', one)
+for s in l:
+    one += f((set([*s[:len(s)//2]]) & set([*s[len(s)//2:]])).pop())
 for i in range(2, len(l), 3):
-    s1 = set(); [s1.add(c) for c in l[i - 2]]
-    s2 = set(); [s2.add(c) for c in l[i - 1]]
-    s3 = set(); [s3.add(c) for c in l[i]]
-    two += f((s1 & s2 & s3).pop())
+    two += f((set([*l[i - 2]]) & set([*l[i - 1]]) & set([*l[i]])).pop())
+print('Part One', one)
 print('Part Two', two)
