0b5dcf10d52144ebff95b8623c7f904c0425fa65
stxo7YYA <119558047+stxo7YYA@users.noreply.github.com>
2022-12-06 07:55:29 +0000

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,12 +1,5 @@
-f = open('input', 'r')
-calories = 0
-top = []
-for line in f.readlines():
-    if line == '\n':
-        top.append(calories)
-        calories = 0
-        continue
-    calories += int(line)
-top.sort()
-print('Part One', max(top))
-print('Part Two', sum(top[-3:]))
+f = open('input', 'r').read()[:-1]
+lst = [sum([int(x) for x in x.strip().split('\n')]) for x in f.split('\n\n')]
+lst.sort()
+print('Part One', max(lst))
+print('Part Two', sum(lst[-3:]))
