4876fb28f9592d4f03530eb5d587d378de894aac
stxo7YYA <119558047+stxo7YYA@users.noreply.github.com>
2022-12-06 07:02:47 +0000

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,17 +1,10 @@
-f = open('input', 'r').readlines()[0][:-1]
-for i in range(len(f)):
-    if i + 3 < len(f):
-        s = set()
-        for j in range(0, 4):
-            s.add(f[i + j])
-        if len(s) == 4:
-            print('Part One', i + 4)
-            break
-for i in range(len(f)):
-    if i + 13 < len(f):
-        s = set()
-        for j in range(0, 14):
-            s.add(f[i + j])
-        if len(s) == 14:
-            print('Part Two', i + 14)
-            break
+f = list(open('input', 'r'))[0][:-1]
+
+def solution(n):
+    for i in range(len(f)):
+        if i + n <= len(f):
+            if len(set(f[i:i + n])) == n:
+                return i + n
+
+print('Part One', solution(4))
+print('Part Two', solution(14))
