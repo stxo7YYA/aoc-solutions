e908823aaf651e32e49375709faa2d8ce352cd2f
stxo7YYA <119558047+stxo7YYA@users.noreply.github.com>
2022-12-06 07:06:45 +0000

    Update 6.py

--- 6.py
+++ 6.py
@@ -2,9 +2,8 @@ f = list(open('input', 'r'))[0][:-1]
 
 def solution(n):
     for i in range(len(f)):
-        if i + n <= len(f):
-            if len(set(f[i:i + n])) == n:
-                return i + n
+        if i + n <= len(f) and len(set(f[i:i + n])) == n:
+            return i + n
 
 print('Part One', solution(4))
 print('Part Two', solution(14))
