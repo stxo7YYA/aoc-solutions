36386375ba8fdaecac02e5fe0ed0552de6659eda
stxo7YYA <119558047+stxo7YYA@users.noreply.github.com>
2022-12-07 08:43:11 +0000

    Update 7.py

--- 7.py
+++ 7.py
@@ -5,47 +5,32 @@ cache = dict()
 
 for x in f:
     y = x.split('\n')
-    cmd = y[0].split()
+    cmd = y.pop(0).split()
 
-    # Update path
+    ''' Update path '''
     if cmd[0] == 'cd':
-        if cmd[1] == '..':
-            path.pop()
-        else:
-            path.append(cmd[1])
+        path.pop() if cmd[1] == '..' else path.append(cmd[1])
 
-    # Process file sizes
+    ''' Process file sizes '''
     if cmd[0] == 'ls':
 
-        # Determine the total size of visible files
+        ''' Determine the total size of visible files '''
         size = 0
-        for i in range(1, len(y) - 1):
-            if not y[i]:
+        for z in y:
+            if not z:
                 break
-            data = y[i].split()
+            data = z.split()
             if data[0] != 'dir':
                 size += int(data[0])
 
-        # Propagate the total size through the path
+        ''' Propagate the total size through the path '''
         for i in range(len(path)):
-            key = str(path[:len(path) - i])
-            if key in cache:
-                cache[key] += size
-            else:
-                cache[key] = size
+            key = str(path[:len(path) - i]) # Key must be hashable
+            cache.setdefault(key, 0)
+            cache[key] += size
 
-one = 0
-for path in cache:
-    size = cache[path]
-    if size <= 100000:
-        one += size
-print('Part One', one)
-
-required = 30000000
-delete = required - (70000000 - cache["['/']"])
-two = required
-for path in cache:
-    size = cache[path]
-    if size >= delete:
-        two = min(two, size)
-print('Part Two', two)
+print('Part One', sum(filter(lambda n: n <= 100000,
+                             [cache[path] for path in cache])))
+print('Part Two', min(filter(
+    lambda n: n >= 30000000 - (70000000 - cache["['/']"]),
+    [cache[path] for path in cache])))
